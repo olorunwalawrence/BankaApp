ad709df1f5b9565ae2c9c9fbe8cf56f21b51bbdc
olorunwalawrence <olorunwalawrence5@gmail.com>
2019-04-13 13:46:01 -0700

    ch(setup): install dependencies and setup express - babel-cli - babel-core - babel-loader - body-parser - dot-env - express [Delivers #165331585]

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+// import apiRoute from './server/routes/authRoutes';
+
+
+const app = express();
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+const print = console;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use('/api/v1', route);
+// app.use('/api/v1', apiRoute);
+app.get('/api/v1', (req, res) => res.status(200).json({
+  message: 'this is the application home page'
+}));
+
+app.route('/*').all((req, res) => res.status(404).json({
+  status: 404,
+  error: '404 Route not found'
+}));
+
+app.listen(port, () => {
+  print.log('server is up and running');
+});
+
+export default app;
