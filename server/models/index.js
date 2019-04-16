fff68d26e550d74bd30e53d0e8e93dbc6bb58bc9
olorunwalawrence <olorunwalawrence5@gmail.com>
2019-04-16 06:52:36 -0700

    ch(setup): setup database config - setup database config [Finishes 165381780]

---
+++ server/models/index.js
@@ -0,0 +1,18 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+import connect from '../config/config';
+
+dotenv.config();
+
+const { production } = connect;
+
+const pool = new Pool(production);
+
+pool.connect().then(() => {
+}).catch((err) => {
+ 
+  console.log(err.message);
+});
+
+
+export default pool;
