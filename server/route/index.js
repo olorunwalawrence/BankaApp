40a4015dcd6b796d6288a86ef7cf6b3e2ce626af
olorunwalawrence <olorunwalawrence5@gmail.com>
2019-04-23 13:14:33 -0700

    feat(delete):create delete - create delete functionality [Delivers 165544931]

--- server/route/index.js
+++ server/route/index.js
@@ -9,19 +9,19 @@ import transactions from '../Controllers/transaction';
 
 const router = express.Router();
 const { createUser, userLogin } = user;
-const { createAccount } = account;
+const { createAccount, } = account;
 const { viewHistory, viewAtransaction } = transactions;
 const { exitingUsername, existingEmail } = findExisting;
 const { signupValidator, loginValidator, accountValidator } = validator;
 const {
-  ActivatOrDeactivateAccct, deleteAccount, creaditAccount, debitAccount, adminUpdateUserRole
+  ActivatOrDeactivateAccct, deleteAccounts, creaditAccount, debitAccount, adminUpdateUserRole
 } = adminController;
 
 router.post('/auth/signup', signupValidator, existingEmail, exitingUsername, createUser);
 router.post('/auth/login', loginValidator, userLogin);
 router.post('/accounts', accountValidator, verifyUser, createAccount);
 router.patch('/account/:accountNumber', verifyUser, ActivatOrDeactivateAccct);
-router.delete('/account/:accountNumber', verifyUser, deleteAccount);
+router.delete('/account/:accountNumber', verifyUser, deleteAccounts);
 router.post('/transactions/:accountNumber/credit', verifyUser, creaditAccount);
 router.post('/transactions/:accountNumber/debit', verifyUser, debitAccount);
 router.get('/accounts/:accountnumber/transactions', verifyUser, viewHistory);
