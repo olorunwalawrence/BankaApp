7a248699f169d431f39b3154f2c8c0a4bbd7cfaa
olorunwalawrence <44552263+olorunwalawrence@users.noreply.github.com>
2019-04-25 05:41:51 -0700

    Update userController.js

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,6 +1,6 @@
 /* eslint-disable require-jsdoc */
 import { config } from 'dotenv';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import db from '../models/index';
 import insert from '../queries/insert';
